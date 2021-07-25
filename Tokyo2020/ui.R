e012263a26ea801c398c458cd5a1b236eff1a67c
ekurtz49 <47067666+ekurtz49@users.noreply.github.com>
2021-07-25 09:33:01 -0500

    let's let Ryan try

--- Tokyo2020/ui.R
+++ Tokyo2020/ui.R
@@ -1,33 +1,23 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
+# ELQ
 #
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-library(rsconnect)
+
+
+
+#library(rsconnect)
 library(shiny)
+library(ggplot2)
+library(rvest)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Greater Kurtz Family Tokyo 2020 Standings"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+
     
     # Show a plot of the generated distribution
     mainPanel(
        plotOutput("distPlot")
     )
-  )
 ))
