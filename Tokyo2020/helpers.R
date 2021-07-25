4f2667bcd8ad54340d5567c081ba5793df3d3a85
ekurtz49 <47067666+ekurtz49@users.noreply.github.com>
2021-07-25 09:24:25 -0500

    for the helper fuckers

---
+++ Tokyo2020/helpers.R
@@ -0,0 +1,29 @@
+# two helper functions
+
+
+
+
+# Function: scrape a single URL
+getCountryMedals = function(url) {
+  rvPage = read_html(url)
+  tableNode = html_node(rvPage, "table#medal-standing-table")
+  if (class(tableNode) == "xml_missing") {
+    return(c(0,0,0))
+  }
+  else {
+    medalTable = html_table(tableNode)
+    gsbVector = as.numeric(medalTable[2,(ncol(medalTable)-4):(ncol(medalTable)-2)])
+    gsbVector[is.na(gsbVector)] = 0
+    return(gsbVector)
+  }
+}
+
+# Function: scrape all the URLs
+updateMedalsAndPoints = function(rankData) {
+  for (i in 1:(length(unique(rankData$scrape)))) {
+    url = paste0(rankData$scrapeRoot[1],rankData$scrape[(i*3-2)])
+    rankData$medals[(i*3-2):(i*3)] = getCountryMedals(url)
+  }
+  rankData$points = rankData$medals*c(3,2,1,6,4,2,20,10,5)
+  return(rankData)
+}
